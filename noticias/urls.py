5f16301c55fc06e3b3ca6763743177d2685109b5
sacrac <crocha09.09@gmail.com>
2013-10-15 16:58:51 -0600

    fix urls noticias

--- noticias/urls.py
+++ noticias/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, url
-from noticias.views import NoticiasList, NoticiasDetailView
+from noticias.views import NoticiasList, NoticiasDetailView, filtro_categoria
 
 urlpatterns = patterns('noticias.views',
     url(r'^lista/$', view=NoticiasList.as_view(), 
                      name='noticias_lista'),
         
     url(r'^(?P<slug>[-_\w]+)/$', view=NoticiasDetailView.as_view(), 
-                                 name='noticias_detalle'),    
+                                 name='noticias_detalle'),
+
+    url(r'^categoria/(?P<id>\d+)/$', 'filtro_categoria', name='noticias_categoria'),   
         
     )

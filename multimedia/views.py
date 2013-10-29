9ff40b936f1f8b88aab0a899b8aa89540c4984e7
sacrac <crocha09.09@gmail.com>
2013-10-29 17:00:41 -0600

    fix views

--- multimedia/views.py
+++ multimedia/views.py
@@ -5,19 +5,19 @@ from .models import *
 class FotosListView(ListView):
     template_name = 'multimedia/fotos_list.html'
     model = Fotos
-    paginate_by = 4
+    paginate_by = 6
 
 class VideosListView(ListView):
     template_name = 'multimedia/videos_list.html'
     model = Videos
-    paginate_by = 8
+    paginate_by = 6
 
 class AudioListView(ListView):
     template_name = 'multimedia/audio_list.html'
     model = Audio
-    paginate_by = 8
+    paginate_by = 6
 
 class AdjuntoListView(ListView):
     template_name = 'multimedia/adjuntos_list.html'
     model = Adjuntos
-    paginate_by = 8
+    paginate_by = 6

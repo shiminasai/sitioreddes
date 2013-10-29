6d2c03401315c01a8b0431e9e5eb4930b0d8d5af
sacrac <crocha09.09@gmail.com>
2013-10-28 22:10:01 -0600

    agregando pais

--- noticias/forms.py
+++ noticias/forms.py
@@ -3,11 +3,12 @@ from captcha.fields import CaptchaField
 from socios.models import Pais
 
 class ContactForm(forms.Form):
-	nombre = forms.CharField(max_length=200)
-	correo = forms.EmailField()
-	asunto = forms.CharField(max_length=200, required=False)
-	mensaje = forms.CharField(required=False, widget=forms.Textarea)
-	captcha = CaptchaField()
+    nombre = forms.CharField(max_length=200)
+    pais = forms.ModelChoiceField(queryset=Pais.objects.all(), required=False)
+    correo = forms.EmailField()
+    asunto = forms.CharField(max_length=200, required=False)
+    mensaje = forms.CharField(required=False, widget=forms.Textarea)
+    captcha = CaptchaField()
 
 class PaisForm(forms.Form):
     pais = forms.ModelChoiceField(queryset=Pais.objects.all())

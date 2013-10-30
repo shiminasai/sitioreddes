eab82a4b8b43600cb9af501910baa5b96187fdbf
sacrac <crocha09.09@gmail.com>
2013-10-30 12:14:56 -0600

    fix publicaciones

--- sitioreddes/context.py
+++ sitioreddes/context.py
@@ -1,9 +1,11 @@
 from eventos.models import Categoria
 from taggit.models import Tag
 from socios.models import Pais
+from publicaciones.models import Publicaciones
 
 def globales(request):
     categorias = Categoria.objects.all()
     tags = Tag.objects.all()
     paises = Pais.objects.all()
-    return {'categorias':categorias, 'tags':tags, 'paises':paises}
+    publi = Publicaciones.objects.order_by('-id')[:3]
+    return {'categorias':categorias, 'tags':tags, 'paises':paises,'publi':publi}

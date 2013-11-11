98d3bd764551bcc6f13c0b20ba2758af0d5d93fe
sacrac <crocha09.09@gmail.com>
2013-11-11 16:59:52 -0600

    fix vies

--- noticias/views.py
+++ noticias/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Noticias, InicioTexto
 from multimedia.models import Videos, Audio, Fotos, Adjuntos
